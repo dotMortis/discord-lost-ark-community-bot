a47f5910dd1fc788b00f2d5b54380d093657270e
Nick K <29275165+dotMortis@users.noreply.github.com>
2022-06-27 11:56:52 +0200

    update schema add EventLog + Event.isDeleted

--- src/db/prisma-client.ts
+++ src/db/prisma-client.ts
@@ -5,6 +5,63 @@ const PRISMA_CLIENT = new PrismaClient({
     log: staticConfig().loggingLevel
 });
 
+//#region Event softdelete
+PRISMA_CLIENT.$use(async (params, next) => {
+    if (params.model === 'Event') {
+        if (params.action === 'findUnique' && params.args.where.deleteAt == null) {
+            params.action = 'findFirst';
+            params.args.where.isDeleted = false;
+        } else if (params.action === 'findFirst' && params.args.where.deleteAt == null) {
+            params.args.where.isDeleted = false;
+        } else if (params.action === 'findMany') {
+            if (params.args.where == null) {
+                params.args.where = {
+                    isDeleted: false
+                };
+            } else if (params.args.where.deleteAt == null) {
+                params.args.where.isDeleted = false;
+            }
+        }
+    }
+    return next(params);
+});
+
+PRISMA_CLIENT.$use(async (params, next) => {
+    if (params.model === 'Event') {
+        if (params.action === 'update' && params.args.where.deleteAt == null) {
+            params.action = 'updateMany';
+            params.args.where.isDeleted = false;
+        }
+        if (params.action === 'updateMany') {
+            if (params.args.where == null) {
+                params.args.where = { isDeleted: false };
+            } else if (params.args.where.deleteAt == null) {
+                params.args.where.isDeleted = false;
+            }
+        }
+    }
+    return next(params);
+});
+
+PRISMA_CLIENT.$use(async (params, next) => {
+    if (params.model === 'Event') {
+        if (params.action == 'delete') {
+            params.action = 'update';
+            params.args.data = { isDeleted: true };
+        }
+        if (params.action === 'deleteMany') {
+            params.action = 'updateMany';
+            if (params.args.data != null) {
+                params.args.data.isDeleted = true;
+            } else {
+                params.args.data = { isDeleted: true };
+            }
+        }
+    }
+    return next(params);
+});
+//#endregion
+
 export const prismaClient = PRISMA_CLIENT;
 
 export const connectPrismaClient = () => PRISMA_CLIENT.$connect();
